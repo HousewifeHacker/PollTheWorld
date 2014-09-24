ef1eef310de8fd41aad316d647db214282cb2545
Jessie Anderson <housewifehacker@gmail.com>
2014-09-24 08:21:01 -0700

    AnswerChoices belongs to polls in parsing of JSON

--- app/assets/javascripts/models/poll.js
+++ app/assets/javascripts/models/poll.js
@@ -1,3 +1,20 @@
 PollApp.Models.Poll = Backbone.Model.extend({
-  urlRoot: '/api/polls'
+  urlRoot: '/api/polls',
+  
+  answerChoices: function() {
+    if (!this.answerChoices) {
+      this._answerChoices = 
+        new PollApp.Collections.AnswerChoices([], { poll: this });
+    }
+    return this._answerChoices;
+  },
+  
+  parse: function(response) {
+    if(response.answerChoices) {
+      this.answerChoices().set(response.answerChoices, { parse: true });
+      delete response.answerChoices;
+    }
+    
+    return response;
+  }
 });
